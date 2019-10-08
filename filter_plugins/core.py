5abeb0216ab18edaac653715f6fd3974d609a6ce
Tom Stockwell <tstockwell@redhat.com>
2019-10-08 14:59:30 +1100

    fixup! wip: upload

--- filter_plugins/core.py
+++ filter_plugins/core.py
@@ -3,15 +3,9 @@ __metaclass__ = type
 
 from ansible.plugins.filter.urlsplit import split_url
 
+
 def slugify(arg, slug='-'):
-    if isinstance(arg, list):
-        return map(_slug, arg, slug)
-    else:
-        return _slug(arg, slug)
-
-def _slug(arg, slug):
     return split_url(arg, 'path').lstrip('/').replace('/', slug)
-
 
 
 class FilterModule(object):
