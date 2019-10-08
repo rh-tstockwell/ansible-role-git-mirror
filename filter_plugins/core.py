28ae6526b799b536e05087b592d1ee00bc39f651
Tom Stockwell <tstockwell@redhat.com>
2019-10-08 17:14:48 +1100

    fixup! wip: upload

--- filter_plugins/core.py
+++ filter_plugins/core.py
@@ -8,9 +8,21 @@ def slugify(arg, slug='-'):
     return split_url(arg, 'path').lstrip('/').replace('/', slug)
 
 
+# builds a map of src repo slugs to their destination
+def repo_map(mirrors, default_repo, slug='-'):
+    def mapped = {}
+    for mirror in mirrors:
+        def repo_slug = slugify(mirror['src'])
+        mapped[repo_slug] = mirror.has_key('dest') mirror['dest'] else '{}/{}'.format()default_repo + '/' + 
+    return mapped
+        
+    return { ): mirror.has_key('dest') mirror.dest else default_repo +  for mirror in mirrors}
+
+
 class FilterModule(object):
     def filters(self):
         return {
-            'slugify': slugify
+            'slugify': slugify,
+            'repo_map': repo_map
         }
 
